9f82a262ce1f53b5cc1dfbfef06df3843ecd4bc8
Dragon <suzuki.r.ak@m.titech.ac.jp>
2013-01-09 14:55:15 +0900

    検索コマンドを実装

--- src/jp/bcat/CLICommand.java
+++ src/jp/bcat/CLICommand.java
@@ -1,1 +1,1 @@
-package jp.bcat;import java.io.*;public interface CLICommand {	public void process(BufferedReader in, PrintWriter out)		throws IOException;}
+package jp.bcat;import java.io.*;public interface CLICommand {	public void process(BufferedReader in, PrintWriter out) throws IOException;}
---
+++ src/jp/bcat/SearchCommand.java
@@ -0,0 +1,32 @@
+package jp.bcat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SearchCommand implements CLICommand {
+	protected BookCatalog catalog;
+	
+	public SearchCommand(BookCatalog catalog) {
+		this.catalog = catalog;
+	}
+
+	@Override
+	public void process(BufferedReader in, PrintWriter out) throws IOException {
+		out.print("キーワードを入力してください: ");
+		out.flush();
+		Book[] result = catalog.searchBooks(in.readLine());	
+		if (result.length == 0) {
+			out.print("キーワードに一致する蔵書が見つかりませんでした。");
+		} else {
+			out.println("検索結果: " + result.length + "項目");
+			BookWriter writer = new BookWriter(out, " ");
+			for (int i = 0; i < result.length; i++) {
+				out.println("[" + (i+1) + "]");
+				out.println("図書ID: " + result[i].getBookId());
+				writer.write(result[i]);
+				out.println();
+			}
+		}
+	}
+}
