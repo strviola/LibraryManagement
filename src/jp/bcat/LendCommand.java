0126bd902c5f730439d492cb8fa285ac2ba5404c
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2013-01-09 23:08:14 +0900

    貸し出し処理の実装

---
+++ src/jp/bcat/LendCommand.java
@@ -0,0 +1,38 @@
+package jp.bcat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LendCommand implements CLICommand {
+	private BookCatalog catalog;
+	
+	public LendCommand(BookCatalog catalog) {
+		this.catalog = catalog;
+	}
+
+	@Override
+	public void process(BufferedReader in, PrintWriter out) throws IOException {
+		out.print("借りたい図書ID: ");
+		out.flush();
+		Book bookToLend = catalog.getBook(in.readLine());
+		if (bookToLend == null) {
+			out.println("該当する図書がありません。");
+			return;
+		}
+		new BookWriter(out, "- ").write(bookToLend);
+		if (bookToLend.getStatus()) {
+			// 図書が貸し出し中
+			out.println("その図書は貸し出し中です。");
+			return;
+		}
+		out.print("この図書を貸し出します。よろしいですか? [y/n]");
+		if (! "y".equals(in.readLine())) {
+			out.println("キャンセルされました。");
+			return;
+		}
+		bookToLend.setStatus(true);
+		out.println("貸し出しが完了しました。");
+	}
+
+}
