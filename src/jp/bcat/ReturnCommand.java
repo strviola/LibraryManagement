412b9ab1e1f67c3ebb0b4986f0e2fc3137f8bab1
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2013-01-09 23:11:00 +0900

    返却コマンドの実装

---
+++ src/jp/bcat/ReturnCommand.java
@@ -0,0 +1,37 @@
+package jp.bcat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ReturnCommand implements CLICommand {
+	private BookCatalog catalog;
+	
+	public ReturnCommand(BookCatalog catalog) {
+		this.catalog = catalog;
+	}
+
+	@Override
+	public void process(BufferedReader in, PrintWriter out) throws IOException {
+		out.print("•Ô‹p‚µ‚½‚¢}‘ID: ");
+		out.flush();
+		Book bookToLend = catalog.getBook(in.readLine());
+		if (bookToLend == null) {
+			out.println("ŠY“–‚·‚é}‘‚ª‚ ‚è‚Ü‚¹‚ñB");
+			return;
+		}
+		new BookWriter(out, "- ").write(bookToLend);
+		if (! bookToLend.getStatus()) {
+			out.println("‚»‚Ì}‘‚Í‘İ‚µo‚µ’†‚Å‚Í‚ ‚è‚Ü‚¹‚ñB");
+			return;
+		}
+		out.print("‚±‚Ì}‘‚ğ•Ô‹p‚µ‚Ü‚·B‚æ‚ë‚µ‚¢‚Å‚·‚©? [y/n]");
+		if (! "y".equals(in.readLine())) {
+			out.println("ƒLƒƒƒ“ƒZƒ‹‚³‚ê‚Ü‚µ‚½B");
+			return;
+		}
+		bookToLend.setStatus(false);
+		out.println("•Ô‹p‚ªŠ®—¹‚µ‚Ü‚µ‚½B");
+	}
+
+}
