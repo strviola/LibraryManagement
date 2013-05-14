7e5e50ab8c99e8b63bf227c306beede8539a0f6d
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2013-05-14 13:33:50 +0900

    External-file Execution

--- src/jp/bcat/MainExecute.java
+++ src/jp/bcat/MainExecute.java
@@ -5,18 +5,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import junit.framework.TestCase;
-import org.junit.Test;
 
-public class MainTest extends TestCase {
-	
-	@Test
-	public void testIntegrated() throws IOException{
+public class MainExecute {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(System.getProperty("user.dir"));
 		String filename = "testinput.txt";
 		BufferedReader in = new BufferedReader(new FileReader(filename));
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
-		Main main = new Main();
-		System.out.println(in);
+		Main main = new Main(true);
 		try {
 			main.process(in, out);
 		} catch (IOException e) {
